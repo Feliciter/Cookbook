35eb1d7fbcecadfa80ec63a73b33c8459bbde4d4
Feliciter <padakluev@gmx.com>
2020-07-13 22:56:25 +0300

    fix issues

--- src/app/components/add-recipe/add-recipe.component.ts
+++ src/app/components/add-recipe/add-recipe.component.ts
@@ -1,8 +1,8 @@
-import { Router } from '@angular/router';
-import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { MatChipInputEvent  } from '@angular/material/chips';
-import { ApiService } from './../../shared/api.service';
+import { Router } from "@angular/router";
+import { Component, OnInit, ViewChild, NgZone } from "@angular/core";
+import { COMMA, ENTER } from "@angular/cdk/keycodes";
+import { MatChipInputEvent } from "@angular/material/chips";
+import { ApiService } from "./../../shared/api.service";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 export interface Subject {
@@ -10,23 +10,21 @@ export interface Subject {
 }
 
 @Component({
-  selector: 'app-add-recipe',
-  templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.css']
+  selector: "app-add-recipe",
+  templateUrl: "./add-recipe.component.html",
+  styleUrls: ["./add-recipe.component.css"],
 })
-
 export class AddRecipeComponent implements OnInit {
   visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
-  @ViewChild('chipList') chipList;
-  @ViewChild('resetRecipeForm') myNgForm;
+  @ViewChild("chipList") chipList;
+  @ViewChild("resetRecipeForm") myNgForm;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   recipeForm: FormGroup;
   subjectArray: Subject[] = [];
-  // SectioinArray: any = ['A', 'B', 'C', 'D', 'E'];
-  selected ={};
+  selected = {};
 
   ngOnInit() {
     this.submitBookForm();
@@ -37,17 +35,15 @@ export class AddRecipeComponent implements OnInit {
     private router: Router,
     private ngZone: NgZone,
     private recipeApi: ApiService
-  ) { }
+  ) {}
 
-  /* Reactive book form */
+  /* Reactive form */
   submitBookForm() {
     this.recipeForm = this.fb.group({
-      recipe_name: ['', [Validators.required]],
-      recipe_description: ['', [Validators.required]],
-      
-      subjects: [this.subjectArray]
-    
-    })
+      recipe_name: ["", [Validators.required]],
+      recipe_description: ["", [Validators.required]],
+      subjects: [this.subjectArray],
+    });
   }
 
   /* Add dynamic languages */
@@ -55,43 +51,26 @@ export class AddRecipeComponent implements OnInit {
     const input = event.input;
     const value = event.value;
     // Add language
-    if ((value || '').trim() && this.subjectArray.length < 5) {
-      this.subjectArray.push({ name: value.trim() })
+    if ((value || "").trim() && this.subjectArray.length < 50) {
+      this.subjectArray.push({ name: value.trim() });
     }
     // Reset the input value
     if (input) {
-      input.value = '';
+      input.value = "";
     }
   }
-
-  // /* Remove dynamic languages */
-  // remove(subject: Subject): void {
-  //   const index = this.subjectArray.indexOf(subject);
-  //   if (index >= 0) {
-  //     this.subjectArray.splice(index, 1);
-  //   }
-  // }  
-
-  /* Date */
-  formatDate(e) {
-    var convertDate = new Date(e.target.value).toISOString().substring(0, 10);
-    this.recipeForm.get('dob').setValue(convertDate, {
-      onlyself: true
-    })
-  }  
 
   /* Get errors */
   public handleError = (controlName: string, errorName: string) => {
     return this.recipeForm.controls[controlName].hasError(errorName);
-  }  
+  };
 
   /* Submit book */
   submitRecipeForm() {
     if (this.recipeForm.valid) {
-      this.recipeApi.AddRecipe(this.recipeForm.value).subscribe(res => {
-        this.ngZone.run(() => this.router.navigateByUrl('/recipe-list'))
+      this.recipeApi.AddRecipe(this.recipeForm.value).subscribe((res) => {
+        this.ngZone.run(() => this.router.navigateByUrl("/recipe-list"));
       });
     }
   }
-
 }
--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,71 +1,58 @@
-import { Recipe } from './../../shared/recipe';
-import { ApiService } from './../../shared/api.service';
-import { Component, ViewChild, OnInit, } from '@angular/core';
-//import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
-
+import { Recipe } from "./../../shared/recipe";
+import { ApiService } from "./../../shared/api.service";
+import { Component, ViewChild, OnInit } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
-
 export class RecipeListComponent implements OnInit {
   RecipeData: any = [];
-  
+
   dataSource: MatTableDataSource<Recipe>;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator; 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
- 
+  displayedColumns: string[] = [
+    "recipe_name",
+    "recipe_description",
+    "created",
+    "dtlm",
+    "action",
+  ];
 
-  displayedColumns: string[] = [ 'recipe_name', 'recipe_description','created','dtlm','action'];
+  constructor(private recipeApi: ApiService) {}
 
-  constructor(private recipeApi: ApiService) {
-  
+  ngOnInit() {
+    this.recipeApi.GetRecipes().subscribe((data) => {
+      this.RecipeData = data;
+      this.dataSource = new MatTableDataSource<Recipe>(this.RecipeData);
+
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    });
   }
 
-  
-
-  ngOnInit() {     
-  
-   this.recipeApi.GetRecipes().subscribe(data => {
-    this.RecipeData = data;
-    this.dataSource = new MatTableDataSource<Recipe>(this.RecipeData);      
-   
-    setTimeout(() => {
-      this.dataSource.paginator = this.paginator;               
-    
-    }, 0);  
-
-  })     
-    
+  ngOnDestroy(): void {
+    if (this.dataSource) {
+      this.dataSource.disconnect();
     }
+  }
 
-  
-
-
-  // ngOnDestroy(): void {
-  //   if (this.dataSource) { this.dataSource.disconnect(); }
-  // }
-  
-
-
-  deleteRecipe(index: number, e){
-    if(window.confirm('Are you sure')) {
+  deleteRecipe(index: number, e) {
+    if (window.confirm("Are you sure")) {
       const data = this.dataSource.data;
-      data.splice((this.paginator.pageIndex * this.paginator.pageSize) + index, 1);
+      data.splice(
+        this.paginator.pageIndex * this.paginator.pageSize + index,
+        1
+      );
       this.dataSource.data = data;
-      this.recipeApi.DeleteRecipe(e._id).subscribe()
+      this.recipeApi.DeleteRecipe(e._id).subscribe();
     }
   }
-
-
-  ///
-
-  
-
 }
